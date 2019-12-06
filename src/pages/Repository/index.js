1ce544cfaf4a2859f59b961e6fd05b3ec41fc7e0
guaracyalima <guaracyaraujolima@gmail.com>
2019-12-06 10:37:18 -0200

    adiciona styled components

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Repostory() {
+  return <h1>Reposytory</h1>;
+}
+
+export default Repostory;
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+	BrowserRouter,
+	Switch,
+	Route
+} from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+	return ( < BrowserRouter > < Switch >
+		<
+		Route path = "/"
+		exact = "exact"
+		component = {
+			Main
+		}
+		/> <
+		Route path = "/repository"
+		component = {
+			Repository
+		}
+		/> < /
+		Switch > < /BrowserRouter > )
+	}
